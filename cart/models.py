bdecf81511b64474774a5b51815c294b9306a093
Bimo Prasetyo Afif <bimo.prasetyoafif@gmail.com>
2019-12-26 13:05:04 +0700

    add cart app

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from allUsers.models import Account
+from products.models import Products
 
 # Create your models here.
+class Cart(models.Model):
+    cartId = models.AutoField(primary_key=True, editable=False, unique=True)
+    CustomerId = models.ForeignKey(Account, related_name="InCart", on_delete=models.CASCADE)
+    productId = models.ForeignKey(Products,related_name="InCart" ,on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+
