4380c1e3ba77f63af96d9418d584c992dab1f5be
Bimo Prasetyo Afif <bimo.prasetyoafif@gmail.com>
2019-12-26 16:57:55 +0700

    db to azure

--- BackupDatabaseRPL/settings.py
+++ BackupDatabaseRPL/settings.py
@@ -112,26 +112,26 @@ WSGI_APPLICATION = 'BackupDatabaseRPL.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'sql_server.pyodbc',
-#         'NAME': 'rpl',
-#         'USER': 'rplyahut',
-#         'PASSWORD': 'bu1ldup!',
-#         'HOST': 'dicodingserverbimo.database.windows.net',
-#         'PORT': '',
-#         'OPTIONS': {
-#             'driver': 'ODBC Driver 13 for SQL Server',
-#         },
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'sql_server.pyodbc',
+        'NAME': 'rpl',
+        'USER': 'rplyahut',
+        'PASSWORD': 'bu1ldup!',
+        'HOST': 'dicodingserverbimo.database.windows.net',
+        'PORT': '',
+        'OPTIONS': {
+            'driver': 'ODBC Driver 17 for SQL Server',
+        },
+    }
+}
 
 # DATABASES = {
 #     'default': {
