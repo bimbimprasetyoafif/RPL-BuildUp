cfbc3468b63a205f55c56513b641e91092f0cd93
Bimo Prasetyo Afif <bimo.prasetyoafif@gmail.com>
2019-12-26 18:53:10 +0700

    ganti odbc 17 ke 13

--- BackupDatabaseRPL/settings.py
+++ BackupDatabaseRPL/settings.py
@@ -119,20 +119,6 @@ WSGI_APPLICATION = 'BackupDatabaseRPL.wsgi.application'
 #     }
 # }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'sql_server.pyodbc',
-#         'NAME': 'rpl',
-#         'USER': 'rplyahut',
-#         'PASSWORD': 'bu1ldup!',
-#         'HOST': 'dicodingserverbimo.database.windows.net',
-#         'PORT': '',
-#         'OPTIONS': {
-#             'driver': 'ODBC Driver 17 for SQL Server',
-#         },
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'sql_server.pyodbc',
@@ -143,7 +129,7 @@ DATABASES = {
         'PORT': '',
         'OPTIONS': {
         #    'driver': 'SQL Server Native Client 11.0',
-            'driver': 'ODBC Driver 17 for SQL Server',
+            'driver': 'ODBC Driver 13 for SQL Server',
         },
     },
 }
