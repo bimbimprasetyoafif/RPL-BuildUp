38178507c5844fb2ce478fc666b4ce30afacd385
Bimo Prasetyo Afif <bimo.prasetyoafif@gmail.com>
2019-12-26 14:01:47 +0700

    updateing design model

--- design/models.py
+++ design/models.py
@@ -1,3 +1,23 @@
 from django.db import models
-
+from django.conf import settings
+from category.models import CategorysHome
 # Create your models here.
+
+class Design(models.Model):
+    DesignId = models.AutoField(primary_key=True, editable=False)
+    category = models.ForeignKey(CategorysHome, related_name="DesignInCategory", on_delete=models.CASCADE)
+    DesignName = models.CharField(max_length=50)
+    DesignDesc = models.CharField(max_length=500)
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='allDesign', on_delete=models.CASCADE, null=True, blank=True)
+
+class RAB(models.Model):
+    RABId = models.AutoField(primary_key=True,editable=False)
+    WorkName = models.CharField(max_length=50)
+    PriceTotal = models.IntegerField()
+
+class DesignImages(models.Model):
+    DesignId = models.ForeignKey(Design, related_name="allImagesDesign", on_delete=models.CASCADE,)
+    Image = models.ImageField(blank=False, null=False)
+
+    def __str__(self):
+        return "{} - {}".format(self.DesignId, self.Image)
