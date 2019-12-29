b8f631bd22d8b23fc3b1798f3d5376ffbe662624
Bimo Prasetyo Afif <bimo.prasetyoafif@gmail.com>
2019-12-29 21:19:11 +0700

    heroku config try

--- BackupDatabaseRPL/settings.py
+++ BackupDatabaseRPL/settings.py
@@ -186,7 +186,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 STATICFILES_DIRS = (
     os.path.join(PROJECT_DIR, 'media'),
 )
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 import dj_database_url 
 prod_db  =  dj_database_url.config(conn_max_age=500)
