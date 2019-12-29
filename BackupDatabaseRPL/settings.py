7084fb77e7ac65bf72d474df50e569c9982d6406
Bimo Prasetyo Afif <bimo.prasetyoafif@gmail.com>
2019-12-29 20:44:25 +0700

    coba heroku

--- BackupDatabaseRPL/settings.py
+++ BackupDatabaseRPL/settings.py
@@ -177,8 +177,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_ROOT = '/'
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = '/'
 STATIC_URL = '/static/'
 MEDIA_URL =  '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'media'),
+)
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
