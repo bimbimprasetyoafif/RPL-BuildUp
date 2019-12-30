671ead0b7e3a3fba1765a94b7a9a48470e4174af
anidast <anidwi.ad1@gmail.com>
2019-12-30 16:44:40 +0700

    Activity Keranjang

--- Android/app/src/main/java/com/example/buildup/ProdukFragment.java
+++ Android/app/src/main/java/com/example/buildup/ProdukFragment.java
@@ -4,10 +4,14 @@ package com.example.buildup;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
 
 
 /**
@@ -15,6 +19,9 @@ import android.view.ViewGroup;
  */
 public class ProdukFragment extends Fragment {
 
+    private RecyclerView mRecycleView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     public ProdukFragment() {
         // Required empty public constructor
@@ -25,7 +32,23 @@ public class ProdukFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_produk, container, false);
+        final View rootView;
+        rootView = inflater.inflate(R.layout.fragment_produk, container, false);
+
+        ArrayList<Item> listItem = new ArrayList<>();
+        listItem.add(new Item(R.drawable.mitrarenov, "Semen", "Tiga Roda", "Rp. 200.000,-"));
+        listItem.add(new Item(R.drawable.mitrarenov, "Semen", "Gresik", "Rp. 100.000,-"));
+        listItem.add(new Item(R.drawable.mitrarenov, "Cat", "Avitex", "Rp. 50.000,-"));
+
+        mRecycleView = rootView.findViewById(R.id.recyclerView);
+        mRecycleView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this.getContext());
+        mAdapter = new Adapter(listItem);
+
+        mRecycleView.setLayoutManager(mLayoutManager);
+        mRecycleView.setAdapter(mAdapter);
+
+        return rootView;
     }
 
 }
