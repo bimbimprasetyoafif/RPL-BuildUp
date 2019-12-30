78ff26a72a1ac9ff73309e7bad6ac236142d7bb2
Bimo Prasetyo Afif <bimo.prasetyoafif@gmail.com>
2019-12-31 00:37:00 +0700

    material

--- design/serializers.py
+++ design/serializers.py
@@ -15,9 +15,51 @@ class DesignRABSerializer(serializers.ModelSerializer):
         model = RAB
         fields = "__all__"
 
+class MaterialAtapSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = MaterialAtap
+        fields = "__all__"
+
+class MaterialPlafonSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = MaterialPlafon
+        fields = "__all__"
+
+class MaterialLantaiSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = MaterialLantai
+        fields = "__all__"
+
+class MaterialDindingSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = MaterialDinding
+        fields = "__all__"
+
+class MaterialCatInteriorSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = MaterialCatDalam
+        fields = "__all__"
+
+class MaterialCatExteriorSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = MaterialCatLuar
+        fields = "__all__"
+
 class DesignSerializer(serializers.ModelSerializer):
     allImagesDesign= DesignImagesSerializer(many=True, read_only=True)
     allRAB= DesignRABSerializer(many=True, read_only=True)
+    CustomAtap= MaterialAtapSerializer(many=True, read_only=True)
+    CustomPlafon= MaterialPlafonSerializer(many=True, read_only=True)
+    CustomLantai= MaterialLantaiSerializer(many=True, read_only=True)
+    CustomDinding= MaterialDindingSerializer(many=True, read_only=True)
+    CustomCatDalam= MaterialCatInteriorSerializer(many=True, read_only=True)
+    CustomCatLuar= MaterialCatExteriorSerializer(many=True, read_only=True)
 
     class Meta:
         model = Design
@@ -25,38 +67,3 @@ class DesignSerializer(serializers.ModelSerializer):
 
 # adaddadadadad
 
-class MaterialAtapSerializer(serializers.ModelSerializer):
-    CustomAtap= DesignSerializer(many=True, read_only=True)
-    class Meta:
-        model = MaterialAtap
-        fields = "__all__"
-
-class MaterialPlafonSerializer(serializers.ModelSerializer):
-    CustomPlafon= DesignSerializer(many=True, read_only=True)
-    class Meta:
-        model = MaterialPlafon
-        fields = "__all__"
-
-class MaterialLantaiSerializer(serializers.ModelSerializer):
-    CustomLantai= DesignSerializer(many=True, read_only=True)
-    class Meta:
-        model = MaterialLantai
-        fields = "__all__"
-
-class MaterialDindingSerializer(serializers.ModelSerializer):
-    CustomDinding= DesignSerializer(many=True, read_only=True)
-    class Meta:
-        model = MaterialDinding
-        fields = "__all__"
-
-class MaterialCatInteriorSerializer(serializers.ModelSerializer):
-    CustomCatDalam= DesignSerializer(many=True, read_only=True)
-    class Meta:
-        model = MaterialCatDalam
-        fields = "__all__"
-
-class MaterialCatExteriorSerializer(serializers.ModelSerializer):
-    CustomCatLuar= DesignSerializer(many=True, read_only=True)
-    class Meta:
-        model = MaterialCatLuar
-        fields = "__all__"
