f35b99bcbc8ec1a7d67e0bfb1fe438df9e04e020
Bimo Prasetyo Afif <bimo.prasetyoafif@gmail.com>
2019-12-30 19:37:58 +0700

    categorys def str

--- category/models.py
+++ category/models.py
@@ -6,11 +6,11 @@ class Categorys(models.Model):
     CategoryName = models.CharField(max_length=50)
 
     def __str__(self):
-        return self.CategoryName
+        return "{} - {}".format(self.CategoryId, self.CategoryName)
 
 class CategorysHome(models.Model):
     CategoryId = models.AutoField(auto_created=True, primary_key=True, editable=False)
     CategoryName = models.CharField(max_length=50)
 
     def __str__(self):
-        return self.CategoryName
+        return "{} - {}".format(self.CategoryId, self.CategoryName)
