f0cb16cf71341bae76cf05970cd5bc34c5733d6b
Bimo Prasetyo Afif <bimo.prasetyoafif@gmail.com>
2019-12-31 21:19:14 +0700

    set url

--- BackupDatabaseRPL/settings.py
+++ BackupDatabaseRPL/settings.py
@@ -180,12 +180,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-# STATIC_ROOT = '/'
-STATIC_ROOT = os.path.join(BASE_DIR, "media")
+STATIC_ROOT = '/'
+# STATIC_ROOT = os.path.join(BASE_DIR, "media")
 
 STATIC_URL = '/static/'
 MEDIA_URL =  '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = os.path.join(PROJECT_DIR, "media")
 STATICFILES_DIRS = (
     os.path.join(PROJECT_DIR, 'media'),
 )
