c2847914c1cf5ab56fb4b91955ccbb1d31e94a85
Bimo Prasetyo Afif <bimo.prasetyoafif@gmail.com>
2019-12-31 13:17:11 +0700

    remove auth

--- BackupDatabaseRPL/settings.py
+++ BackupDatabaseRPL/settings.py
@@ -73,12 +73,12 @@ AUTHENTICATION_BACKENDS = (
     )
 
 REST_FRAMEWORK = {
-    # 'DEFAULT_AUTHENTICATION_CLASSES': (
-    #     'rest_framework.authentication.TokenAuthentication',
-    # ),
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
     ),
+    # 'DEFAULT_PERMISSION_CLASSES': (
+    #     'rest_framework.permissions.IsAuthenticated',
+    # ),
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 200
